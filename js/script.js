6a0e43a8d8c6b7de6cc9d0e3e75db5c2b5a5cf08
XeniaPanayot <30495419+XeniaPanayot@users.noreply.github.com>
2017-12-09 00:47:45 +0100

    links in catalogue-inners, logo tag catalogue paddings, arrows up&down

---
+++ js/script.js
@@ -0,0 +1,15 @@
+			var write = document.querySelector(".write-us-button");
+			var popup = document.querySelector(".write-inner");
+			var close = document.querySelector(".modal-close");
+			var yourname = popup.querySelector("[name=name]");
+
+			write.addEventListener("click", function (evt) {
+				evt.preventDefault();
+				popup.classList.add("write-inner-show");
+				yourname.focus();
+			});
+
+			close.addEventListener("click", function (evt) {
+				evt.preventDefault();
+				popup.classList.remove("write-inner-show");
+			});
