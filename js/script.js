4bf5dc0d4199bf3d17e8e63f20af44467265df7d
XeniaPanayot <30495419+XeniaPanayot@users.noreply.github.com>
2017-12-09 02:35:21 +0100

    index&catalogue paddings, js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-			var write = document.querySelector(".write-us-button");
-			var popup = document.querySelector(".write-inner");
-			var close = document.querySelector(".modal-close");
-			var yourname = popup.querySelector("[name=name]");
+var write = document.querySelector(".write-us-button");
+var popup = document.querySelector(".write-inner");
+var close = document.querySelector(".modal-close");
+var yourname = popup.querySelector("[name=name]");
 
-			write.addEventListener("click", function (evt) {
-				evt.preventDefault();
-				popup.classList.add("write-inner-show");
-				yourname.focus();
-			});
+write.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("write-inner-show");
+	yourname.focus();
+});
 
-			close.addEventListener("click", function (evt) {
-				evt.preventDefault();
-				popup.classList.remove("write-inner-show");
-			});
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("write-inner-show");
+});
